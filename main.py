de4a35a50f25da8bb8dcee9845cbf210096fb551
Renushri Rawat <renushri.rawat@gmail.com>
2022-07-09 13:29:45 +0000

    Visual of marathon

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Hello");
+import time
+
+person_forward = '''
+     O
+  / /\_,
+___/\ 
+   /_ 
+'''
+
+person_backward = '''
+     O
+  / /\_,
+   /\ 
+ /   \\
+'''
+
+running = [person_forward, person_backward]
+
+def clear(sleep_time):
+    time.sleep(sleep_time)
+    print("\033c")
+
+def print_with_time(string, sleep_time):
+    print(string)
+    print("˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙")
+    clear(sleep_time)
+
+clear(0)
+
+speed = 10
+for i in range(100):
+    sleep_time = 1/speed
+    print_with_time(running[i%2], sleep_time)
+    if speed <= 1:
+        continue
+    speed = speed - 0.1
+
